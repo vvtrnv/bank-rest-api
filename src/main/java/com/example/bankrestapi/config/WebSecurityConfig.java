5805b359230aab79742caf48bd6aad5d0f860b3d
Vladimir <swatik07@yandex.ru>
2023-01-15 20:46:58 +0700

    Added web security config

--- src/main/java/com/example/bankrestapi/config/WebSecurityConfig.java
+++ src/main/java/com/example/bankrestapi/config/WebSecurityConfig.java
@@ -1,4 +1,61 @@
 package com.example.bankrestapi.config;
 
-public class WebSecurityConfig extends {
+import com.example.bankrestapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    UserService userService;
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                    //Доступ только для не зарегистрированных пользователей
+                    .requestMatchers("/registration").not().fullyAuthenticated()
+                    //Доступ только для пользователей с ролью Администратор
+                    .requestMatchers("/admin/**").hasRole("ADMIN")
+                    .requestMatchers("/main").hasRole("USER")
+                    //Доступ разрешен всем пользователей
+                    .requestMatchers("/", "/resources/**").permitAll()
+                //Все остальные страницы требуют аутентификации
+                .anyRequest().authenticated()
+                .and()
+                    //Настройка для входа в систему
+                    .formLogin()
+                    .loginPage("/login")
+                    //Перенарпавление на главную страницу после успешного входа
+                    .defaultSuccessUrl("/")
+                    .permitAll()
+                .and()
+                    .logout()
+                    .permitAll()
+                    .logoutSuccessUrl("/");
+
+        return http.build();
+    }
+
+    @Autowired
+    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder());
+    }
 }
